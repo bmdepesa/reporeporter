4d99077927728f41f2c8f746ed210624fcd93377
Brandon DePesa <brandon.depesa@rancher.com>
2021-09-19 12:25:56 -0700

    Quick fix on autofilter

--- buildreport.py
+++ buildreport.py
@@ -16,7 +16,7 @@ def main():
 		ws = wb.create_sheet(file)
 		for row in csv.reader(open(curfile), delimiter="\t"):
 			ws.append([_convert_to_number(cell) for cell in row])
-			ws.auto_filter.ref = ws.dimensions
+		ws.auto_filter.ref = ws.dimensions
 
 	wb.remove(wb['Sheet'])
 	wb.save("reports/report-" + args.date + ".xlsx")
